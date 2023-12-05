c93f30edfc28c45851087ded4a54eaa4a75f920f
Nadjim Karimi <nadjim73@gmail.com>
2023-12-05 11:27:26 +0100

    lab dom race car

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game; // added
 
   startButton.addEventListener("click", function () {
-    startGame();
+    startGame()
   });
-
+    
+  document.addEventListener('click',()=>{
+    console.log('click')
+    });
+  
   function startGame() {
     console.log("start game");
+    game= new Game(); // added
+    game.start(); // added
+    gameloop();
+  }
+
+  function gameLoop() {
+    game.player.move();
+    requestedAnimationFrame(gameLoop);
   }
 };
