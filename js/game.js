35e4d82c00d1abba0e738c229856520bb274f9ca
Nadjim Karimi <nadjim73@gmail.com>
2023-12-06 01:47:48 +0100

    lab-dom-race car done

--- js/game.js
+++ js/game.js
@@ -1,37 +1,88 @@
 class Game {
     constructor() {
-this.startScreen = document.getElementById('game-intro');
-this.gameScreen = document.getElementById('game-screen');
-this.gameEndScreen = document.getElementById('game-end');
-this.player = null;
-this.height = 600;
-this.width = 500
-this.obstacles = [];
-this.score = 0;
-this.lives = 3;
-this.gameIsOver = false;
+      this.startScreen = document.getElementById('game-intro');
+      this.gameScreen = document.getElementById('game-screen');
+      this.gameEndScreen = document.getElementById('game-end');
+      this.player = null;
+      this.height = 600;
+      this.width = 500;
+      this.obstacles = [];
+      this.score = 0;
+      this.lives = 3;
+      this.gameIsOver = false;
     }
+  
     start() {
-        this.gameScreen.style.height = `${this.height}px`;
-        this.gameScreen.style.width = `${this.width}px`;
-    
-        this.startScreen.style.display = 'none';
-        this.gameScreen.style.display = 'block';
-    
-        this.gameLoop();
-}
-gameLoop() {
-    console.log("in the game loop");
-    (if this.gameIsOver)
-    {return;}
-    this.update();
-    window.requestAnimationFrame
-    (() => { 
+      this.gameScreen.style.height = `${this.height}px`;
+      this.gameScreen.style.width = `${this.width}px`;
+  
+      this.startScreen.style.display = 'none';
+      this.gameScreen.style.display = 'block';
+  
+      this.player = new Player(
+        this.gameScreen,
+        200,
+        500,
+        100,
+        150,
+        "./images/car.png"
+      );
+  
+      this.gameLoop();
+    }
+  
+    gameLoop() {
+      console.log("in the game loop");
+      if (this.gameIsOver) {
+        return;
+      }
+  
+      this.update();
+  
+      window.requestAnimationFrame(() => {
         this.gameLoop();
       });
+    }
+  
+    update() {
+      this.player.move();
+      console.log("in the update");
+  
+      if (Math.random() < 0.01) {
+        const newObstacle = new Obstacle();
+        this.obstacles.push(newObstacle);
+      }
+  
+      this.obstacles.forEach((obstacle) => {
+        obstacle.move();
+  
+        if (obstacle.collidesWith(this.player)) {
+          const obstacleIndex = this.obstacles.indexOf(obstacle);
+          this.obstacles.splice(obstacleIndex, 1);
+          this.player.reduceLives();
+  
+          if (this.player.getLives() === 0) {
+            this.endGame();
+          }
+        }
+  
+        if (obstacle.isOffscreen()) {
+          const obstacleIndex = this.obstacles.indexOf(obstacle);
+          this.obstacles.splice(obstacleIndex, 1);
+          this.player.increaseScore();
+        }
+      });
+    }
+  
+    endGame() {
+      this.player.element.remove();
+  
+      this.obstacles.forEach((obstacle) => {
+        obstacle.element.remove();
+      });
+  
+      this.gameIsOver = true;
+      this.gameScreen.style.display = "none";
+      this.gameEndScreen.style.display = "block";
+    }
   }
-  update() {
-    console.log("in the update");
-  }
-
- }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  restartButton.addEventListener('click', function() {
+    restartGame();
+  });
+  function restartGame() {
+    location.reload();
+  }
   let game; // added
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
 
   startButton.addEventListener("click", function () {
     startGame()
@@ -15,7 +52,7 @@ window.onload = function () {
     console.log("start game");
     game= new Game(); // added
     game.start(); // added
-    gameloop();
+    gameLoop();
   }
 
   function gameLoop() {
